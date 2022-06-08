617965fc7f0b6d5710f1e25ea876c82646db4c82
Aman <asingh99222@gmail.com>
2022-06-08 12:19:36 +0530

    Create findUniqe.cpp

---
+++ OperartionOnArray/findUniqe.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int findUnique(int ar[], int size){
+    sort(ar, ar+size);
+    
+    for(int i=0; i<size; i = i+2){
+        if(ar[i] != ar[i+1]){
+            // cout<<"unique Element in array is: "<<ar[i]<<endl;
+            return ar[i];
+        }
+    }
+
+    return 54132;
+}
+
+int betterUnique(int ar[], int size){
+    int ans = 0;
+    for(int i=0; i<size; i++){
+        ans ^= ar[i];
+        cout<<ans<<" "<<endl;
+    }
+    cout<<"\n\n\n\n";
+    return ans;
+}
+
+int main(){
+    int ar[] = {2,3,1,6,3,6,2};
+    int size = sizeof(ar)/sizeof(ar[0]);
+   
+
+    cout<<"Unique element: "<<betterUnique(ar, size)<<endl;
+
+    return 0;
+}
