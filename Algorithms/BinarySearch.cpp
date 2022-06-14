79fe0da762faf2284a16a5831bf21ca33a890aeb
Aman <asingh99222@gmail.com>
2022-06-14 13:38:29 +0530

    implemented Binary Search Algorithm

---
+++ Algorithms/BinarySearch.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int value, int* a, int size){
+
+    int mid = size/2;
+    int l = 0, u = size-1;
+
+    while(l<=u){
+        // cout<<a[1mid]<<" ";
+
+        if(a[mid] == value){
+            return mid;
+        }
+
+        if(a[mid]<value){
+            l = mid+1;
+        }
+        else {
+            u = mid-1;
+        }
+
+        mid = l + ((u-l)/2);
+    }
+
+    return -1;
+}
+
+int main(){
+    int a[] = {2,4,8,11,13,33,44,62};
+    int size = sizeof(a)/sizeof(a[0]);
+    cout<<binarySearch(8, a, size)<<endl;
+    // cout<<5/2<<endl;
+    return 0;
+}
