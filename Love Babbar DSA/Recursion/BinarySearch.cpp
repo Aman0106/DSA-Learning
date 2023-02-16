5a1e24be70c3ab41a05ace5ad9d566f6b78ede7a
Aman0106 <56467973+Aman0106@users.noreply.github.com>
2023-02-16 19:25:02 +0530

    Binary Search using recursion

---
+++ Love Babbar DSA/Recursion/BinarySearch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int *arr, int start, int end, int target){
+
+    if(start > end) return -1;
+
+    int mid = ((end-start)/2) + start;
+    
+    if(arr[mid] == target) return mid;
+    
+    if(arr[mid] < target) start = mid + 1;
+    
+    if(arr[mid] > target) end = mid - 1;
+
+    return binarySearch(arr, start, end, target);
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,30};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<endl<<binarySearch(arr, 0, size-1, 19)<<endl;
+
+    return 0;
+}
